f241f5b9a1615cefdb793a259efb10687102feb5
reyeszonya <77856869+reyeszonya@users.noreply.github.com>
2022-04-16 23:40:03 -0400

    Add files via upload

---
+++ PenguinTables.sql
@@ -0,0 +1,47 @@
+Use PenguinDatabase
+
+CREATE TABLE customer (
+ [id] [int] PRIMARY KEY IDENTITY(1,1) NOT NULL,
+ [firstName] NVARCHAR(20),
+ [lastName] NVARCHAR(20),
+ [address] NVARCHAR(200),
+ [mobile] NVARCHAR(15),
+ [email] NVARCHAR(50))
+
+CREATE TABLE employee (
+ [id] [int] PRIMARY KEY IDENTITY(1,1) NOT NULL,
+ [firstName] NVARCHAR(20),
+ [lastName] NVARCHAR(20),
+ [address] NVARCHAR(200),
+ [mobile] int,
+ [email] NVARCHAR(50),
+ [position] NVARCHAR(20),
+ [employmentDate] int,
+ [activeStatus] NVARCHAR(20))
+
+ CREATE TABLE vehicle (
+ [id] [int] PRIMARY KEY IDENTITY(1,1) NOT NULL,
+ [VIN] NVARCHAR(50),
+ [licensePlate] NVARCHAR(20),
+ [model] NVARCHAR(50),
+ [manufacturer] NVARCHAR(50),
+ [year] int,
+ [color] NVARCHAR(50),
+ [customerid] [int] FOREIGN KEY REFERENCES customer(id))
+
+ CREATE TABLE repairSchedule (
+ [id] [int] PRIMARY KEY IDENTITY(1,1) NOT NULL,
+ [scheduleDate] int,
+ [timeFrom] int,
+ [timeTo] NVARCHAR(200),
+ [plannedRepair] NVARCHAR(200),
+ [actualRepair] NVARCHAR(50),
+ [employeeid] [int] FOREIGN KEY REFERENCES employee(id))
+
+ CREATE TABLE  repairTasks (
+ [id] [int] PRIMARY KEY IDENTITY(1,1) NOT NULL,
+ [taskName] NVARCHAR(50),
+ [taskDescription] NVARCHAR(200),
+ [duration] NVARCHAR(50),
+ [taskPrice] int,
+ [scheduleid] [int] FOREIGN KEY REFERENCES repairSchedule(id))
